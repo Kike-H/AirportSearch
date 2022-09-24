0322d6078ee94a138c730bdc0bfad545d01a3e77
Manuel E. Hernandez <mehernandezduarte@gmail.com>
2022-09-24 11:06:53 -0500

    Initial Commit

---
+++ AirportSearch/AirportSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportSearchApp.swift
+//  AirportSearch
+//
+//  Created by Kike Hernandez D. on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct AirportSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
