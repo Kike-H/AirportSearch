e1f8299412e0e026d98720550637820d516db931
Manuel E. Hernandez <mehernandezduarte@gmail.com>
2022-09-25 13:19:04 -0500

    Make view for dont found airports ➕

--- AirportSearch/UIComponent/ListComponent.swift
+++ AirportSearch/UIComponent/ListComponent.swift
@@ -11,8 +11,20 @@ struct ListComponent: View {
     var airports: AirportModel
     
     var body: some View {
-        List (airports) { airport in
-            CellComponent(airportName: airport.name, countryCode: airport.countryCode, lat: airport.lat, lon: airport.lon)
+        if(airports.isEmpty) {
+            HStack {
+                Text("NO AIRPORTS FOUND")
+                    .font(.title)
+                    .foregroundColor(.gray)
+                Image(systemName: "exclamationmark.triangle.fill")
+                    .font(.title)
+                    .foregroundColor(.gray)
+            }
+        }
+        else {
+            List (airports) { airport in
+                CellComponent(airportName: airport.name, countryCode: airport.countryCode, lat: airport.lat, lon: airport.lon)
+            }
         }
     }
 }
