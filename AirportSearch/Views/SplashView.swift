a89a40ca41fc4f41544df2a05bd19ce584df1082
Manuel E. Hernandez <mehernandezduarte@gmail.com>
2022-09-25 23:03:54 -0500

    Add SplashScreen ➕

---
+++ AirportSearch/Views/SplashView.swift
@@ -0,0 +1,35 @@
+//
+//  SplashView.swift
+//  AirportSearch
+//
+//  Created by Kike Hernandez D. on 25/09/22.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @State private var isActive = false
+    var body: some View {
+        if(isActive) {
+            ContentView()
+        }
+        else {
+            VStack{
+                Image("Logo")
+                Text("by KIKE-H")
+                    .font(.title2)
+                    .padding(.top, 40)
+            }.onAppear {
+                DispatchQueue.main.asyncAfter(wallDeadline: .now()+2.5) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
